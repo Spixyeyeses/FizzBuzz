de4d044cf84745f09da29b59c7bcbc817f6db0cc
John Shuai <shuaihaowen@gmail.com>
2026-09-15 19:24:46 -0400

    added while loop solution to FizzBuzz, refactored to extract helper method

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -7,13 +7,47 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 100; i++) {
+//        // Solve the lab1.FizzBuzz challenge with for loop.
+//        for (int i = 1; i <= 100; i++) {
+//
+//            // Find out which numbers divide i.
+//            boolean divisibleBy3 = i % 3 == 0;
+//            boolean divisibleBy5 = i % 5 == 0;
+//
+//            // Print our appropriate result.
+//            if (divisibleBy3 && divisibleBy5) {
+//
+//                System.out.println("Fizz Buzz");
+//
+//            } else if (divisibleBy3) {
+//
+//                System.out.println("Fizz");
+//
+//            } else if (divisibleBy5) {
+//
+//                System.out.println("Buzz");
+//
+//            } else {
+//
+//                System.out.println(i);
+//
+//            }
+//        }
+
+        doFizzBuzz();
+    }
+
+    private static void doFizzBuzz() {
+        // Solve the lab1.FizzBuzz challenge with while loop.
+        int j = 0;
+
+        while (j <= 100) {
 
             // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
+            boolean divisibleBy3 = j % 3 == 0;
+            boolean divisibleBy5 = j % 5 == 0;
 
-            // Print our appropriate result.
+            // Print our appropriate result
             if (divisibleBy3 && divisibleBy5) {
 
                 System.out.println("Fizz Buzz");
@@ -28,9 +62,13 @@ class FizzBuzz {
 
             } else {
 
-                System.out.println(i);
+                System.out.println(j);
 
             }
+
+            //Update j
+            j++;
+
         }
     }
 }
